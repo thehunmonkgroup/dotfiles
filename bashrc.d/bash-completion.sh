cb9efd9daabbe94817addb2804ad76c0af39c4f9
Chad Phillips <chad@apartmentlines.com>
2019-01-28 12:59:07 -0800

    make bash-completion boot script more env agnostic

--- bashrc.d/bash-completion.sh
+++ bashrc.d/bash-completion.sh
@@ -1,12 +1,13 @@
+prefix=""
 brewpath=`which brew`
 if [ -n "$brewpath" ]; then
   prefix="$(brew --prefix)"
+fi
 
-  if [ -z "${BASH_COMPLETION_COMPAT_DIR}" ] && [ -d ${HOME}/.bash_completion.d ]; then
-    export BASH_COMPLETION_COMPAT_DIR=${HOME}/.bash_completion.d
-  fi
+if [ -f ${prefix}/etc/bash_completion ]; then
+  . ${prefix}/etc/bash_completion
+fi
 
-  if [ -f ${prefix}/etc/bash_completion ]; then
-    . ${prefix}/etc/bash_completion
-  fi
+if [ -z "${BASH_COMPLETION_COMPAT_DIR}" ] && [ -d ${HOME}/.bash_completion.d ]; then
+  export BASH_COMPLETION_COMPAT_DIR=${HOME}/.bash_completion.d
 fi
