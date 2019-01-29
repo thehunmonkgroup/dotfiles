397d08ef503ce4793345f8e0b73c06d7636c10ae
Chad Phillips <chad@apartmentlines.com>
2019-01-28 20:47:20 -0800

    make nvm bash script compatible with linux

--- bashrc.d/nvm.sh
+++ bashrc.d/nvm.sh
@@ -1,2 +1,8 @@
-export NVM_DIR=~/.nvm
- . $(brew --prefix nvm)/nvm.sh
+if [ -n "`which brew`" ]; then
+  export NVM_DIR="$(brew --prefix nvm)/.nvm"
+else
+  export NVM_DIR="$HOME/.nvm"
+fi
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
