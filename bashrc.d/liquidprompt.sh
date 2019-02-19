42b102b82a925965713de4d3dc918cb654b7b951
Chad Phillips <chad@apartmentlines.com>
2019-02-19 09:55:52 -0800

    update liquidprompt to support ubuntu installs

--- bashrc.d/liquidprompt.sh
+++ bashrc.d/liquidprompt.sh
@@ -1,2 +1,7 @@
-# Only load Liquid Prompt in interactive shells, not from a script or from scp
-[[ $- = *i* ]] && source ~/liquidprompt/liquidprompt
+# Only load liquidprompt in interactive shells, not from a script or from scp
+if [ -f ${HOME}/liquidprompt/liquidprompt ]; then
+  echo $- | grep -q i 2>/dev/null && . ${HOME}/liquidprompt/liquidprompt
+else
+  echo $- | grep -q i 2>/dev/null && . /usr/share/liquidprompt/liquidprompt
+fi
+
