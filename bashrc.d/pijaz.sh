2b518bb82773242ceeb0d5e647de77986daef3f1
Chad Phillips <chad@apartmentlines.com>
2019-07-21 17:52:29 -0700

    move pijaz aliases

--- bashrc.d/pijaz.sh
+++ bashrc.d/pijaz.sh
@@ -1,9 +1,9 @@
 # Pijaz aliases.
-alias pij="cd /usr/local/pijaz"
-alias pijcs="cd /usr/local/pijaz/code-source"
-alias pijcd="cd /usr/local/pijaz/code-deploy"
-alias pijas="cd /usr/local/pijaz/assets-source"
-alias pijad="cd /usr/local/pijaz/assets-deploy"
-alias pcli="LD_LIBRARY_PATH=/usr/local/pijaz/lib /usr/local/pijaz/code-deploy/osx/bin/pijazcli"
+alias pij="cd ${HOME}/git/pijaz"
+alias pijcs="cd ${HOME}/git/pijaz/code-source"
+alias pijcd="cd ${HOME}/git/pijaz/code-deploy"
+alias pijas="cd ${HOME}/git/pijaz/assets-source"
+alias pijad="cd ${HOME}/git/pijaz/assets-deploy"
+alias pcli="LD_LIBRARY_PATH=${HOME}/git/pijaz/lib ${HOME}/git/pijaz/code-deploy/osx/bin/pijazcli"
 
-alias salt="cd /usr/local/pijaz/salt"
+alias salt="cd ${HOME}/git/pijaz/salt"
