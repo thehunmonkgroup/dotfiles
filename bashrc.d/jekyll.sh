0977cec98e4308815c060208b786aeba9086a3ec
Chad Phillips <chad@apartmentlines.com>
2019-11-20 17:55:32 -0800

    clean static site before building

--- bashrc.d/jekyll.sh
+++ bashrc.d/jekyll.sh
@@ -1,2 +1,2 @@
 # Alias to trigger a local site rebuild.
-alias jkb="bundle exec jekyll server -w -I --livereload --config _config.dev.yml"
+alias jkb="bundle exec jekyll clean && bundle exec jekyll server -w -I --livereload --config _config.dev.yml"
