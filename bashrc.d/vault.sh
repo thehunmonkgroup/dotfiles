1e8c3089b0bb8d2a22ee525a4b8e28eea3ad0a2e
Chad Phillips <chad@apartmentlines.com>
2020-02-19 16:13:19 -0800

    extract poor man's vault config

--- bashrc.d/vault.sh
+++ bashrc.d/vault.sh
@@ -3,16 +3,3 @@ vault_bin=`which vault`
 if [ -n "${vault_bin}" ]; then
   complete -C ${vault_bin} vault
 fi
-
-# Bash functions for poor man's Vault.
-vault_funcs="${HOME}/git/stirlab/poor-mans-vault/vault.sh"
-if [ -r "${vault_funcs}" ]; then
-  . ${vault_funcs}
-fi
-
-# Apartment Lines data vault for poor man's Vault.
-vault_dir="${HOME}/git/apartmentlines/vault"
-if [ -r "${vault_dir}" ]; then
-  export VAULT_ENV="development"
-  export VAULT_VAULT_DIR="${vault_dir}"
-fi
