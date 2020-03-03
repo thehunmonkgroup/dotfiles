1f60a92295814b49ed55e4b7e2a5605e24665b7a
Chad Phillips <chad@apartmentlines.com>
2020-03-02 20:55:34 -0800

    vault -> pmv

--- bashrc.d/poor-mans-vault.sh
+++ bashrc.d/poor-mans-vault.sh
@@ -1,11 +1,11 @@
 # Bash functions for poor man's Vault.
-vault_funcs="${HOME}/git/stirlab/poor-mans-vault/vault.sh"
-if [ -r "${vault_funcs}" ]; then
-  . ${vault_funcs}
+pmv_funcs="${HOME}/git/stirlab/poor-mans-vault/pmv.sh"
+if [ -r "${pmv_funcs}" ]; then
+  . ${pmv_funcs}
 fi
 
 # Apartment Lines data vault for poor man's Vault.
 vault_dir="${HOME}/git/apartmentlines/vault"
 if [ -r "${vault_dir}" ]; then
-  export VAULT_VAULT_DIR="${vault_dir}"
+  export PMV_VAULT_DIR="${vault_dir}"
 fi
