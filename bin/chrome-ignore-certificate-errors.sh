dca884253b1b91686830ca352040890738a62b3a
Chad Phillips <chad@apartmentlines.com>
2020-06-04 16:56:23 -0700

    fix chrome ignore cert errors script

--- bin/chrome-ignore-certificate-errors.sh
+++ bin/chrome-ignore-certificate-errors.sh
@@ -2,5 +2,5 @@
 
 # Quick opening of Google Chrome with SSL certificate checks disabled.
 
-open -a "Google Chrome" --args --user-data-dir=/Users/hunmonk/chrome-profiles/stable --ignore-certificate-errors
+chromium --ignore-certificate-errors
 
