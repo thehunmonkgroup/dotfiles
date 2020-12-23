06abe3b16ca805d59bdf2518500636a6c45c0a46
Chad Phillips <chad@apartmentlines.com>
2020-12-23 15:56:26 -0800

    fix env var

--- bin/vim-line-to-task.sh
+++ bin/vim-line-to-task.sh
@@ -4,9 +4,9 @@ DEFAULT_PROJECT="INBOX"
 DEFAULT_TASK="None"
 
 while read line; do
-  if [ -n "${ONENOTE_TASK_UUID}" ]; then
-    description="$(task rc.verbose=nothing ${ONENOTE_TASK_UUID} | grep -m 1 ^Description | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')"
-    project="$(task rc.verbose=nothing ${ONENOTE_TASK_UUID} | grep -m 1 ^Project | awk '{print $2}')"
+  if [ -n "${ONENOTE_TASK}" ]; then
+    description="$(task rc.verbose=nothing ${ONENOTE_TASK} | grep -m 1 ^Description | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')"
+    project="$(task rc.verbose=nothing ${ONENOTE_TASK} | grep -m 1 ^Project | awk '{print $2}')"
   else
     description="${DEFAULT_TASK}"
     project="${DEFAULT_PROJECT}"
