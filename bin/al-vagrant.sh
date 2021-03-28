9579c0208af23bd079fabe60fe45cfb653a8c6ea
Chad Phillips <chad@apartmentlines.com>
2021-03-27 19:04:21 -0600

    script to start/stop all servers in order

---
+++ bin/al-vagrant.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+PROGNAME="$(basename $0)"
+
+function usage() {
+  echo "
+Usage: ${PROGNAME} start | stop
+
+Start/stop all Apartment Lines development servers.
+
+  start: Start servers.
+  stop: Stop servers.
+
+With no arguments, show this help.
+"
+}
+
+vagrant_dir="${HOME}/vagrant"
+op="${1}"
+
+server_list="data1-central.local
+data2-central.local
+gw-admin.local
+gw-central.local
+gw-east.local
+union1-east.local
+voice1-central.local
+web1-central.local
+orch.local
+repo.local
+monitor.local"
+
+if [ $# -ne 1 ] || ( [ "${op}" != "start" ] && [ "${op}" != "stop" ] ); then
+  usage
+  exit 1
+fi
+
+_box_command() {
+  local cmd="${1}"
+  shift
+  local box_list=("$@")
+  for box_dir in "${box_list[@]}"; do
+    echo "Performing command '${cmd}' for box '${box_dir}'"
+    if [ -f "${vagrant_dir}/${box_dir}/Vagrantfile" ]; then
+      cd ${vagrant_dir}/${box_dir}
+      vagrant ${cmd}
+    else
+      echo "ERROR: ${vagrant_dir}/${box_dir} has no Vagrantfile"
+    fi
+  done
+}
+
+reverse() {
+  tac <(echo "$@" | tr ' ' '\n') | tr '\n' ' '
+}
+
+function start_servers() {
+  _box_command up $(echo $server_list)
+}
+
+function stop_servers() {
+  _box_command halt $(reverse $server_list)
+}
+
+case $op in
+  start)
+    start_servers
+    ;;
+  stop)
+    stop_servers
+    ;;
+esac
