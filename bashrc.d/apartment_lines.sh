8658c72328e06b716fb8104c9927a76ad7beacc4
Chad Phillips <chad@apartmentlines.com>
2022-01-07 11:15:51 -0500

    als alias

--- bashrc.d/apartment_lines.sh
+++ bashrc.d/apartment_lines.sh
@@ -4,6 +4,7 @@ alias sc="ssh cerebro.apartmentlines.com"
 # Site aliases.
 alias al="cd ~/git/apartmentlines"
 alias alc="al && cd apartmentlines.com"
+alias als="al && cd apartmentlines-static"
 alias alm="cd ~/git/apartmentlines/apartmentlines.com/sites/all/modules"
 alias iac="al && cd iac"
 alias fsa="al && cd freeswitch-apartmentlines"
