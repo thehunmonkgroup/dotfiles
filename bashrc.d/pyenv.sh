4347b45939f31c51ade3c6049a63ddee947ec81d
Chad Phillips <chad@apartmentlines.com>
2022-04-27 14:08:30 -0400

    fix pyenv init

--- bashrc.d/pyenv.sh
+++ bashrc.d/pyenv.sh
@@ -1,7 +1,10 @@
 export PYENV_ROOT="${HOME}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv > /dev/null; then
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+fi
 pyenv virtualenv-init --help > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   eval "$(pyenv virtualenv-init -)"
