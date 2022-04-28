03ba27c0a5ce9f566263c7cb9f27db6c51066162
Chad Phillips <chad@apartmentlines.com>
2022-04-27 22:46:45 -0400

    replace vit alias with vit bash function

--- bashrc.d/vit.sh
+++ bashrc.d/vit.sh
@@ -1,1 +1,4 @@
-alias vit='cd ~/git/vit && PYTHONPATH=${HOME}/git/vit python vit/command_line.py'
+vit() {
+  cd ~/git/vit && PYTHONPATH=${HOME}/git/vit python vit/command_line.py "$@"
+}
+export -f vit
