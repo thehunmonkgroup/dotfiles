03589f56f5c10299421d79115ea096ff6d357688
Chad Phillips <chad@apartmentlines.com>
2022-06-28 13:20:56 -0400

    support multiple BTC addresses

--- bin/btc-balance.sh
+++ bin/btc-balance.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-DEFAULT_ADDRESS="bc1q6ney3se79j3lqswcg5dr2025kc2548c6p4vec2"
+DEFAULT_ADDRESSES="
+bc1qurc0lf6xsm5ktmrvws0nnpwenf79qkjevpxvnj
+bc1q6ney3se79j3lqswcg5dr2025kc2548c6p4vec2
+"
 
-ADDRESS="${1:-${DEFAULT_ADDRESS}}"
+ADDRESSES="${1:-${DEFAULT_ADDRESSES}}"
 SCALE=8
 BTC_DIVISOR=100000000
+total=0
 
-balance=$(curl -s https://api.blockcypher.com/v1/btc/main/addrs/${ADDRESS} | jq .final_balance)
-decimal_balance=$(echo "scale=${SCALE}; ${balance} / ${BTC_DIVISOR}" | bc -l)
+for address in ${ADDRESSES}; do
+  balance=$(curl -s https://api.blockcypher.com/v1/btc/main/addrs/${address} | jq .final_balance)
+  decimal_balance=$(echo "scale=${SCALE}; ${balance} / ${BTC_DIVISOR}" | bc -l)
+  echo "Balance for ${address}: ${decimal_balance} BTC"
+  total=$(echo "scale=${SCALE}; ${total} + ${decimal_balance}" | bc -l)
+done
 
-echo "Balance for ${ADDRESS}: ${decimal_balance} BTC"
+echo "Total: ${total} BTC"
 
 exit 0
