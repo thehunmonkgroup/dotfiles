76a01d9ade0ec755b217db9f0406475426016e0c
Chad Phillips <chad@apartmentlines.com>
2022-11-25 16:53:47 -0500

    add script to update staging/local databases

---
+++ bin/al-update-staging-database.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+PROGNAME="$(basename $0)"
+
+ORCH_STAGING="orch.al-staging.com"
+UNION_STAGING="union1-east.al-staging.com"
+DATA1_STAGING="data1-central.al-staging.com"
+ORCH_LOCAL="orch.local"
+UNION_LOCAL="union1-east.local"
+DATA1_LOCAL="data1-central.local"
+
+function usage() {
+  echo "
+Usage: ${PROGNAME} help | execute
+
+ Operations on Apartment Lines S3 storage.
+
+  execute: Execute the update action.
+  help: This help.
+
+With no arguments, show this help.
+"
+}
+
+op="${1}"
+
+update_staging_databases() {
+  echo "Migrating production S3 databases to staging..."
+  ${HOME}/bin/al-s3-operations.sh update-staging-databases
+  echo "Testing database connection on ${DATA1_STAGING}..."
+  ssh ${DATA1_STAGING} "mariadb -e 'SELECT 1'"
+  if [ $? -eq 0 ]; then
+    echo "Removing previous data dumps from ${DATA1_STAGING}..."
+    ssh ${DATA1_STAGING} "rm -v /var/db/*.sql.gz"
+    if [ $? -eq 0 ]; then
+      echo "Stopping slave on  ${UNION_STAGING}..."
+      ssh ${UNION_STAGING} "mariadb -e 'STOP SLAVE'"
+      if [ $? -eq 0 ]; then
+        echo "Updating databases on ${DATA1_STAGING}..."
+        ssh ${DATA1_STAGING} "salt-call state.sls server.data.apartmentlines.database"
+        if [ $? -eq 0 ]; then
+          echo "Reconfiguring database replication from ${ORCH_STAGING}..."
+          ssh ${ORCH_STAGING} "salt-run state.orch orch.configure-database-replication"
+          if [ $? -eq 0 ]; then
+            echo "Successfully upgraded staging databases!"
+            exit 0
+          else
+            echo "ERROR: could not reconfigure database replication from ${ORCH_STAGING}"
+          fi
+        else
+          echo "ERROR: could not update databases on ${DATA1_STAGING}, aborting"
+        fi
+      else
+        echo "ERROR: could not stop slave on ${UNION_STAGING}, aborting"
+      fi
+    else
+      echo "ERROR: could not remove previous data dumps on ${DATA1_STAGING}, aborting"
+    fi
+  else
+    echo "ERROR: could not connect to database on ${DATA1_STAGING}, aborting"
+  fi
+  exit 1
+}
+
+update_local_databases() {
+  echo "Starting all local servers..."
+  ${HOME}/bin/al-vagrant.sh start
+  echo "Testing database connection on ${DATA1_LOCAL}..."
+  ssh ${DATA1_LOCAL} "mariadb -e 'SELECT 1'"
+  if [ $? -eq 0 ]; then
+    echo "Removing previous data dumps from ${DATA1_LOCAL}..."
+    ssh ${DATA1_LOCAL} "rm -v /var/db/*.sql.gz"
+    if [ $? -eq 0 ]; then
+      echo "Stopping slave on  ${UNION_LOCAL}..."
+      ssh ${UNION_LOCAL} "mariadb -e 'STOP SLAVE'"
+      if [ $? -eq 0 ]; then
+        echo "Updating databases on ${DATA1_LOCAL}..."
+        ssh ${DATA1_LOCAL} "salt-call state.sls server.data.apartmentlines.database"
+        if [ $? -eq 0 ]; then
+          echo "Reconfiguring database replication from ${ORCH_LOCAL}..."
+          ssh ${ORCH_LOCAL} "salt-run state.orch orch.configure-database-replication"
+          if [ $? -eq 0 ]; then
+            echo "Successfully upgraded local databases!"
+            echo "Stopping all local servers..."
+            ${HOME}/bin/al-vagrant.sh stop
+            echo "Starting standalone server..."
+            ${HOME}/bin/al-vagrant.sh standalone
+            exit 0
+          else
+            echo "ERROR: could not reconfigure database replication from ${ORCH_LOCAL}"
+          fi
+        else
+          echo "ERROR: could not update databases on ${DATA1_LOCAL}, aborting"
+        fi
+      else
+        echo "ERROR: could not stop slave on ${UNION_LOCAL}, aborting"
+      fi
+    else
+      echo "ERROR: could not remove previous data dumps on ${DATA1_LOCAL}, aborting"
+    fi
+  else
+    echo "ERROR: could not connect to database on ${DATA1_LOCAL}, aborting"
+  fi
+  exit 1
+}
+
+case ${op} in
+  execute)
+    update_staging_databases
+    if [ $? -eq 0 ]; then
+      update_local_databases
+    fi
+    ;;
+  help)
+    usage
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
