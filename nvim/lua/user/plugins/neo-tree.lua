655e02f12f9a9ec174d8a7b8b7d421b4fa7b023d
Chad Phillips <chad@apartmentlines.com>
2022-12-29 12:41:58 -0500

    provide line numbers for neotree window

--- nvim/lua/user/plugins/neo-tree.lua
+++ nvim/lua/user/plugins/neo-tree.lua
@@ -1,3 +1,13 @@
 return {
   close_if_last_window = false,
+  event_handlers = {
+    {
+      event = "vim_buffer_enter",
+      handler = function ()
+        if vim.bo.filetype == "neo-tree" then
+          vim.cmd("setlocal number")
+        end
+      end
+    },
+  },
 }
