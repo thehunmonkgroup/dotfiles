726b28257e75407fec7a5dd04ecca6e42fa3b8ee
Chad Phillips <chad@apartmentlines.com>
2022-12-29 22:13:17 -0500

    remove dead nvim config

--- nvim/lua/user/todo_config/nvim-dap-ui.lua
+++
@@ -1,5 +0,0 @@
-local nmap = require('user.util').nmap
-
-require("dapui").setup()
-
-nmap("<Leader>dx", ":lua require('dapui').toggle()<CR>", {silent=true})
--- nvim/lua/user/todo_config/nvim-dap.lua
+++
@@ -1,7 +0,0 @@
-local nmap = require('util').nmap
-
-nmap("<Leader>db", ":lua require('dap').toggle_breakpoint()<CR>", {silent=true})
-nmap("<Leader>dB", ":lua require('dap').set_breakpoint()<CR>", {silent=true})
-nmap("<Leader>lp", ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {silent=true})
-nmap("<Leader>dr", ":lua require('dap').repl.open()<CR>", {silent=true})
-nmap("<Leader>dl", ":lua require('dap').run_last()<CR>", {silent=true})
