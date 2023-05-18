399206fc4e20bf334e640599c963c1d3d0feebdf
Chad Phillips <chad@apartmentlines.com>
2023-05-18 08:57:23 -0400

    cd to chatgpt dir

--- bashrc.d/colossus.sh
+++ bashrc.d/colossus.sh
@@ -14,7 +14,7 @@ alias sec="cd ~/Documents/reference && gocryptfs security.encrypted security && 
 alias fin="cd ~/Documents/reference && gocryptfs finance.encrypted security && cd security"
 
 # ChatGPT wrapper.
-alias chatgpt="chatgpt --log ~/.local/share/chatgpt-wrapper/logs/$(date +%Y-%m-%d).log"
+alias chatgpt="cd ~/git/chatgpt && chatgpt --log ~/.local/share/chatgpt-wrapper/logs/$(date +%Y-%m-%d).log"
 
 ###############################################################################
 # FUNCTIONS
