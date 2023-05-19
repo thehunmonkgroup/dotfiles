6d74b551e75dea25f3d3d374c7e6e165a8fd4e09
Chad Phillips <chad@apartmentlines.com>
2023-05-19 15:17:03 -0400

    add file-based option to disable rvm

--- bashrc.d/rvm.sh
+++ bashrc.d/rvm.sh
@@ -1,3 +1,5 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+if [ ! -f /tmp/disable-rvm ]; then
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+  [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+fi
