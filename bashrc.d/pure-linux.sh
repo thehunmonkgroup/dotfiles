3906e761b9ef0f8d08679ed9e000465a12655c38
Chad Phillips <chad@apartmentlines.com>
2023-12-17 11:37:06 -0500

    fix purevpn startup script

--- bashrc.d/pure-linux.sh
+++ bashrc.d/pure-linux.sh
@@ -1,6 +1,1 @@
-# TODO: Remove this if they ever fix their install script.
-if [ ! -f /tmp/atom-update-resolve-conf ]; then cp -v /opt/pure-linux/atom-update-resolve-conf /tmp/; fi
-if [ ! -f /tmp/atom-update-resolve-conf-wg ]; then cp -v /opt/pure-linux/atom-update-resolve-conf-wg /tmp/; fi
-# TODO: Remove this if they ever fix their install script.
-
 export PATH=$PATH:/opt/pure-linux/bin
