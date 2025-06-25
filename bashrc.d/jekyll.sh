7d63c38e8233324c22c82b7819c26f6c70163553
Chad Phillips <chad@apartmentlines.com>
2025-06-25 12:11:59 -0400

    switch jkb to port 4001

--- bashrc.d/jekyll.sh
+++ bashrc.d/jekyll.sh
@@ -1,5 +1,5 @@
 # Alias to trigger a local site rebuild.
 # With incremental build disabled.
-alias jkb="bundle exec jekyll clean && bundle exec jekyll server -w --livereload --config _config.yml,_config.dev.yml"
+alias jkb="bundle exec jekyll clean && bundle exec jekyll server --port 4001 -w --livereload --config _config.yml,_config.dev.yml"
 # With incremental build enabled.
-alias jkbi="bundle exec jekyll clean && bundle exec jekyll server -w -I --livereload --config _config.yml,_config.dev.yml"
+alias jkbi="bundle exec jekyll clean && bundle exec jekyll server --port 4001 -w -I --livereload --config _config.yml,_config.dev.yml"
